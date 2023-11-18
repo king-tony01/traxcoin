9db8d11f27af1e2f7ff9ecb7b88abb39c00ca027
King Tony <kingtony3825@gmail.com>
2023-11-18 17:25:46 +0100

    First Commit
    The backend is working, including the mailing system.

---
+++ server.js
@@ -0,0 +1,111 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+const { deliverMail } = require("./mailer.js");
+const PORT = process.env.PORT || 3400;
+const server = http.createServer((req, res) => {
+  const { pathname, query } = url.parse(req.url, true);
+  const extension = path.extname(pathname);
+  let contentType;
+  if (pathname.includes(".")) {
+    switch (extension) {
+      case ".css":
+        contentType = "text/css";
+        const cssPath = path.join(__dirname, pathname);
+        fs.readFile(cssPath, "utf-8", (err, data) => {
+          if (err) console.log(err);
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(data);
+        });
+        break;
+      case ".js":
+        contentType = "application/javascript";
+        const jsPath = path.join(__dirname, pathname);
+        fs.readFile(jsPath, "utf-8", (err, data) => {
+          if (err) console.log(err);
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(data);
+        });
+        break;
+      case ".jpeg":
+        contentType = "image/jpeg";
+        const jpegPath = path.join(__dirname, pathname);
+        fs.readFile(jpegPath, "", (err, data) => {
+          if (err) console.log(err);
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(data);
+        });
+        break;
+      case ".png":
+        contentType = "image/png";
+        const pngPath = path.join(__dirname, pathname);
+        fs.readFile(pngPath, "", (err, data) => {
+          if (err) console.log(err);
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(data);
+        });
+        break;
+      case ".jpg":
+        contentType = "image/jpg";
+        const jpgPath = path.join(__dirname, pathname);
+        fs.readFile(jpgPath, "", (err, data) => {
+          if (err) console.log(err);
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(data);
+        });
+        break;
+      case ".JPG":
+        contentType = "image/jpg";
+        const JPGPath = path.join(__dirname, pathname);
+        fs.readFile(JPGPath, "", (err, data) => {
+          if (err) console.log(err);
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(data);
+        });
+        break;
+      case ".svg":
+        contentType = "image/svg+xml";
+        const svgPath = path.join(__dirname, pathname);
+        fs.readFile(svgPath, "", (err, data) => {
+          if (err) console.log(err);
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(data);
+        });
+        break;
+    }
+  }
+
+  if (pathname == "/") {
+    const home = path.join(__dirname, "index.html");
+    fs.readFile(home, "utf-8", (err, data) => {
+      if (err) console.log(err);
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+  if (pathname == "/traxcoin/wallet") {
+    const home = path.join(__dirname, "wallet.html");
+    fs.readFile(home, "utf-8", (err, data) => {
+      if (err) console.log(err);
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+  if (pathname == "/new-wallet") {
+    let body;
+    req.on("data", (chunk) => {
+      body = chunk;
+    });
+    req.on("end", () => {
+      let resData = JSON.parse(body);
+      deliverMail(resData);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ stat: true }));
+    });
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running at port: ${PORT}`);
+});
